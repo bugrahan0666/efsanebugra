a86b94a7e0c21a99efdf11319520591baf94f6ad
Glitch (legendary-keen-cantaloupe) <none>
2020-10-09 02:50:50 +0000

    🍝🍭 Checkpoint ./acarkomutlar/acar-boskomut.js:23472859/1109

--- acarkomutlar/acar-boskomut.js
+++ acarkomutlar/acar-boskomut.js
@@ -7,8 +7,17 @@ exports.run = (client, message, args) => {
   
 let uye = message.mentions.users.first() || message.author;
 let bilgi = db.get(`yetkili.${uye.id}`);
-let erkek = bilgi.erkek || 0 + `Kural İhlali.`;
-let kiz = bilgi.kiz || 0 + `0`;
+if(bilgi.erkek === "1" && bilgi.kiz === "1") {
+var erkek = bilgi.erkek || 0 
+var kiz = bilgi.kiz || 0 
+} else {
+var erkek = "0"
+var kiz = "0"
+}
+var erkek = bilgi.erkek || 0 
+var kiz = bilgi.kiz || 0
+
+
 var ses_suresi = Math.round(db.get(`${uye.id}_sesdedur`)/60)+" dakika"
  const embed = new Discord.RichEmbed()
     .addField(`Yetkili Teyit Bilgilerin`,`Toplam ${erkek} erkek teyit ettin!\nToplam ${kiz} kız teyit ettin!\nToplam ${erkek+kiz} kişi teyit ettin!`)
@@ -26,7 +35,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: "yetkiligptm",
+  name: "ye",
   description: "ACAR Temiz v11 Altyapı",
   usage: ""
 };

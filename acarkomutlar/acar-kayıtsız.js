fec87fbae8136e3e6093b0b8de07c64573607663
Glitch (legendary-keen-cantaloupe) <none>
2020-10-09 19:47:30 +0000

    🎎🍤 Checkpoint ./acarkomutlar/acar-kayıtsız.js:23472859/1979

--- acarkomutlar/acar-kayıtsız.js
+++ acarkomutlar/acar-kayıtsız.js
@@ -3,21 +3,21 @@ const db = require("quick.db")
 const acarayarlar = require('../acar/botayarlari.json');
 let acar = require('../acar/botayarlari.json');
 exports.run = async (client, message, args) => {
- if (!message.member.roles.has(acarayarlar.registercommandid) && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`Bilgi` , `Bu komutu kullanmak için gerekli yetkiye sahip değilsin!`).setColor("2e0101").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp());
+ if (!message.member.roles.has(acarayarlar.registercommandid) && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`${acarayarlar.sunucuadi} Bilgi` , `Bu komutu kullanmak için gerekli yetkiye sahip değilsin!`).setColor("2e0101").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp());
   let kullanıcı = message.mentions.users.first()
-  if (!kullanıcı) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`Bilgi` , `Bir kullanıcı etiketlemelisin!`).setColor("2e0101").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp());
+  if (!kullanıcı) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`${acarayarlar.sunucuadi} Bilgi` , `Bir kullanıcı etiketlemelisin!`).setColor("2e0101").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp());
   let user = message.mentions.users.first();
   let rol = message.mentions.roles.first()
   let member = message.guild.member(kullanıcı)
-   if (member.roles.has(acarayarlar.kayıtsızrol)) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`${acarayarlar.sunucuadi} Hata` , `Kullanıcı zaten kayıtlı tekrardan kayıt edemem!`).setColor("RED").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp())
+   if (member.roles.has(acarayarlar.kayıtsızrol)) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`${acarayarlar.sunucuadi} Hata` , `Kayıtsıza atılanı tekrardan kayıtsıza atamam!`).setColor("RED").setFooter(message.author.tag ,message.author.avatarURL).setTimestamp())
 if(member.roles.has(acarayarlar.erkekrol1)) {
-db.fetch(`yetkili.${message.author.id}`);
-db.add(`yetkili.${message.author.id}.erkek`, -1);
-message.author.send('Bir erkeği kayıtsıza attığın için teyit sıralamana -1 puan yansıdı.')
+    db.fetch(`yetkili.${message.author.id}`);
+    db.add(`yetkili.${message.author.id}.erkek`, -1);
+    message.reply('Bir erkeği kayıtsıza attığın için teyit sıralamana -1 puan yansıdı.')
 } else {
-db.fetch(`yetkili.${message.author.id}`);
-db.add(`yetkili.${message.author.id}.kadın`, -1);
-message.author.send('Bir kadını kayıtsıza attığın için teyit sıralamana -1 puan yansıdı.')
+    db.fetch(`yetkili.${message.author.id}`);
+    db.add(`yetkili.${message.author.id}.kadın`, -1);
+    message.reply('Bir kadını kayıtsıza attığın için teyit sıralamana -1 puan yansıdı.')
 }
  await member.setNickname(`${acarayarlar.tagsiz} ' ${acar.yenibiriisim}`)
       message.guild.members.get(member.id).roles.forEach(r => {

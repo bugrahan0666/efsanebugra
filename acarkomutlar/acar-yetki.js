abd5759aa839e0d20d1e37202b1ba59488d06ed6
Glitch (legendary-keen-cantaloupe) <none>
2020-10-10 21:01:06 +0000

    🎳💏 Checkpoint ./acar/botayarlari.json:23638365/255 ./acarkomutlar/acar-yetki.js:23638365/180

--- acarkomutlar/acar-yetki.js
+++ acarkomutlar/acar-yetki.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 let acar = require('../acar/botayarlari.json');
 exports.run = async(client, message, args) => {
-  var kullanabilecekRol = "761692833144242226"; // komutu kullanabilecek rolün id
+  var kullanabilecekRol = "753603197003890818"; // komutu kullanabilecek rolün id
   if (!message.member.roles.has(kullanabilecekRol)) return;
   if(args[0] !== "yükselt" && args[0] !== "düşür") return message.reply(`\`${this.help.name} yükselt/düşür @üye1, üye2, üye3...\``);
   if(!message.mentions.members.first()) return message.reply('Üyeleri belirtmelisin!');
   if(message.mentions.members.some(x => !x.roles.first || !x.hoistRole)) return message.reply('Etiketlediğin üyeler arasında hiç role sahip olmayan üyeler var!');
 
-  let enÜstYetkiliRolü = "747108059545075802"; // en üst yetkili rolünün id
-  let enAltYetkiliRolü = "736493714859622421"; // en alt yetkili rolünün id
-  let boosterRolü = "734570532929929330"; // sunucu booster rolü varsa idsini girin
+  let enÜstYetkiliRolü = "761513024166166558"; // en üst yetkili rolünün id
+  let enAltYetkiliRolü = "753591494891274261"; // en alt yetkili rolünün id
+  let boosterRolü = "753586786965520444"; // sunucu booster rolü varsa idsini girin
 
   if(args[0] === "yükselt") {
     message.mentions.members.forEach(async x => {
